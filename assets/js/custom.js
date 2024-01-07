e387ecc142ff5157f8e8d795eb2389f05819e450
arifyourdev <hariskhan855855@gmail.com>
2024-01-07 22:41:05 +0530

    add

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -17,4 +17,22 @@ tabs.forEach((tab) => {
     });
 });
 
-
+$(document).ready(function () {
+    $('#hero-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        autoplay: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    });
+});
